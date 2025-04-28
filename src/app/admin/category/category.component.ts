6292e681f6f273a7bb838157e08f7df26c6d8392
can gündoğdu <gundogdu.can@hotmail.com>
2025-04-29 00:09:32 +0300

    29.04.2025

--- src/app/admin/category/category.component.ts
+++ src/app/admin/category/category.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Category } from '../../_models/category';
 import { CategoryService } from '../../_services/category.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-category',
@@ -11,6 +12,8 @@ import { CategoryService } from '../../_services/category.service';
 export class CategoryComponent {
 
 categoryList: Category[];
+category: Category= new Category();
+editCategory: any = {};
 
 
 constructor(private categoryService: CategoryService) {
@@ -24,5 +27,66 @@ getAll(){
   })
 }
 
+create(){
+  this.categoryService.create(this.category).subscribe({
+    next: values => this.categoryList.push(values),
+    error: err => console.log(err),
+    complete: () => {
+      Swal.fire({
+        title: "Başarılı!",
+        text: "Kategori Başarı İle Eklendi.",
+        icon: "success"
+      })
+    }
+  })
+}
+
+onSelected(model:Category){
+  this.editCategory = model;
+}
+
+update(){
+  this.categoryService.update(this.editCategory.id,this.editCategory).subscribe({
+  error: err => console.log(err),
+  complete: () => {
+    Swal.fire({
+      title: "Başarılı!",
+      text: "Kategori Başarı İle Güncellendi.",
+      icon: "success"
+    })
+    this.getAll();
+  }
+  })
+}
+
+delete(id:number){
+  Swal.fire({
+    title: "Silmek İstediğinize Emin Misiniz?",
+    text: "Bu işlemi geri alamazsınız!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Evet, Sil!",
+    cancelButtonText: "Hayır, İptal Et!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+
+
+      this.categoryService.delete(id).subscribe({
+        error: err => console.log(err),
+        complete: () => {
+          Swal.fire({
+            title: "Silindi!",
+            text: "Kategori Başarı İle Silindi.",
+            icon: "success"
+          })
+          this.getAll();
+        }
+      })
+
+    }
+  });
+}
 
 }
